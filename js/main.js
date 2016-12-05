967d3d60fa0b8b997a6c3348c8b487f9a70479a7
Mihael Tomić <mihael.tomich@gmail.com>
2016-12-05 09:58:01 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+//javascript blame - jebiga neznam...
+
+$('.jq-menu').click( {
+    $('.jq-layoutMain').toggleClass('open');
+    $('.jq-layoutAside').toggleClass('open');
+});
+
+$('.jq-menu').click(function() {
+    $('.jq-layoutMain').toggleClass('open');
+    $('.jq-layoutAside').toggleClass('open');
+});
+
+$('.jq-menu').click(function ({
+    $('.jq-layoutMain').toggleClass('open');
+    $('.jq-layoutAside').toggleClass('open');
+}));
